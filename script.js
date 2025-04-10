1de8fcfab8c5f6ff44c75d0616a2f9febdfdef31
LSD <194146983+LSD-NSI@users.noreply.github.com>
2025-04-10 08:05:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ function verifierMotDePasse() {
     var resultat = document.getElementById("resultat");
 
     var longueur = motDePasse.length >= 8;
-    
     var majuscule = /[A-Z]/.test(motDePasse);
-    
     var minuscule = /[a-z]/.test(motDePasse);
-    
     var chiffre = /\d/.test(motDePasse);
-    
+
     var caractere_special = /[.@$!%*?&]/.test(motDePasse);
 
     var force = 0;
@@ -20,13 +17,13 @@ function verifierMotDePasse() {
     if (caractere_special) force += 1;
 
     if (force === 5) {
-        resultat.textContent = "Force du mot de passe : Fort";
+        resultat.textContent = "🔒 Force du mot de passe : Fort";
         resultat.className = "fort";
     } else if (force >= 3) {
-        resultat.textContent = "Force du mot de passe : Moyen";
+        resultat.textContent = "🔐 Force du mot de passe : Moyen";
         resultat.className = "moyen";
     } else {
-        resultat.textContent = "Force du mot de passe : Faible";
+        resultat.textContent = "⚠️ Force du mot de passe : Faible";
         resultat.className = "faible";
     }
 }
